58561dd20bac09010e4304ad0be697c40b0fa355
Nicklas Vedsted <nicklasvedsted@gmail.com>
2022-12-06 15:47:20 +0100

    Make day 6 implementation ~20x faster through ungodly measures

--- src/days/day6.rs
+++ src/days/day6.rs
@@ -1,14 +1,39 @@
-use std::collections::HashSet;
 use crate::day::DaySolver;
 
 pub const DAY_6: DaySolver = DaySolver::Standard { part1: Some(part1), part2: Some(part2) };
 
-fn solver(input: &str, window: usize) -> String {
+type CharacterCount = [usize; char_to_index('z') + 1];
+
+const fn char_to_index(c: char) -> usize {
+    c as usize - 'a' as usize
+}
+
+fn no_duplicates(counts: &CharacterCount) -> bool {
+    counts.iter().cloned().all(|c| c < 2)
+}
+
+fn solver(input: &str, window_size: usize) -> String {
     let characters = input.chars().collect::<Vec<_>>();
 
-    (characters.windows(window).enumerate()
-        .filter(|(_, w)| (HashSet::<char>::from_iter((w).iter().cloned()).len()) == (w.len()))
-        .next().unwrap().0 + window).to_string()
+    let mut counts: CharacterCount = Default::default();
+    characters.iter()
+        .take(window_size).cloned()
+        .map(char_to_index)
+        .for_each(|i| counts[i] += 1);
+
+    if no_duplicates(&counts) {
+        return window_size.to_string();
+    }
+
+    for i in window_size..characters.len() {
+        counts[char_to_index(characters[i - window_size])] -= 1;
+        counts[char_to_index(characters[i])] += 1;
+
+        if no_duplicates(&counts) {
+            return (i + 1).to_string();
+        }
+    }
+    panic!("No solution");
 }
 
 fn part1(input: &str) -> String {
